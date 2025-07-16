d8969909026b7d25b1d4592f2db7e5444a575122
darshan45672 <drshnbhandary@gmail.com>
2025-07-16 22:16:22 +0530

    updated clound front

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ProjectDetails } from './Pages/project-details/project-details';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'project-details', component: ProjectDetails },
+];
