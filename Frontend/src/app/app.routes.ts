042650fa6a0eed73ef759f8d76fb4c718d1cc431
darshan45672 <drshnbhandary@gmail.com>
2025-07-16 22:37:49 +0530

    fix: routes

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ProjectDetails } from './Pages/project-details/project-details';
+import { Home } from './Pages/home/home';
 
 export const routes: Routes = [
+    { path: '', component: Home },
     { path: 'project-details', component: ProjectDetails },
 ];
