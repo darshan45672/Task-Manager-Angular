9c66ce40a7b9da0ea3e33a88e34a4d353d4fbf04
darshan45672 <drshnbhandary@gmail.com>
2025-07-17 19:55:14 +0530

    added routes

--- Frontend/src/app/Pages/home/home.ts
+++ Frontend/src/app/Pages/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Preloader } from '../../ui_components/preloader/preloader';
 import { Sidebar } from '../../ui_components/includes/sidebar/sidebar';
 import { BottomNavBar } from '../../ui_components/includes/bottom-nav-bar/bottom-nav-bar';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [Preloader, Sidebar, BottomNavBar],
+  imports: [Preloader, Sidebar, BottomNavBar, RouterModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
