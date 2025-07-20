bbf2ed16a3d9f53d5ad97c193e525760340d922f
darshan45672 <drshnbhandary@gmail.com>
2025-07-20 12:44:16 +0530

    added component recent project

--- Frontend/src/app/Pages/home/home.ts
+++ Frontend/src/app/Pages/home/home.ts
@@ -4,10 +4,11 @@ import { Sidebar } from '../../ui_components/includes/sidebar/sidebar';
 import { BottomNavBar } from '../../ui_components/includes/bottom-nav-bar/bottom-nav-bar';
 import { RouterModule } from '@angular/router';
 import { TodaysTask } from '../../ui_components/tasks/todays-task/todays-task';
+import { RecentProject } from '../../ui_components/tasks/recent-project/recent-project';
 
 @Component({
   selector: 'app-home',
-  imports: [Preloader, Sidebar, BottomNavBar, RouterModule, TodaysTask],
+  imports: [Preloader, Sidebar, BottomNavBar, RouterModule, TodaysTask, RecentProject],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
